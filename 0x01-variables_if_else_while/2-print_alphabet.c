8a488f6b9d242c5f900f06be0b9a5b942c902326
vic-kip <vcherutich19@gmail.com>
2023-06-16 14:42:38 -0700

    prints a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char g;
+
+	g = 'a';
+	while
+		(g <= 'z') {
+			putchar(g);
+			g++;
+			}
+		putchar('\n');
+		return (0);
+}
