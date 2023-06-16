e9ba50439791130ecb125a3a5574ccabe90fb692
vic-kip <vcherutich19@gmail.com>
2023-06-16 14:53:58 -0700

    print a-z in both small and capital letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+
+	char y;
+
+	x = 'a';
+	y = 'A';
+	while 
+		(x <= 'z') {
+			putchar(x);
+			x++;
+		}
+	while 
+		(y <= 'Z') {
+			putchar (y);
+			y++;
+		}
+	putchar('\n');
+	return (0);
+}
