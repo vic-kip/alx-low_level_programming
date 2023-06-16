7b597effdda353714dddb7fcf814f4bfe86e6677
vic-kip <vcherutich19@gmail.com>
2023-06-16 15:15:30 -0700

    print a to z without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 97;
+	
+	while (x <=122)
+	{
+		if (x ==101 || x == 113)
+		{
+			x++;
+			continue;
+		}
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
