b812fd990ecf04409e3dce7d57c085a0fe7da618
vic-kip <vcherutich19@gmail.com>
2023-06-16 15:46:34 -0700

    print all numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b = 0; b <= 9; b++)
+	{
+		putchar(b + 48);
+	}
+	putchar('\n');
+	return (0);
+}
