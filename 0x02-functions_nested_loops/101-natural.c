1982c73f0e46b85fd8d5b1a6fe43591e73be751b
vic-kip <vcherutich19@gmail.com>
2023-06-20 14:25:57 -0700

    a program that prints the first 50 Fibonacci numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -15,12 +15,12 @@ int main(void)
 		if (i == 0)
 		printf("%ld", j);
 	else if (i == 1)
-		printf("%ld", k);
+		printf(", %ld", k);
 	else
 	{
 		k += j;
 		j = k - j;
-		printf(",%ld", k);
+		printf(", %ld", k);
 	}
 		++i;
 	}
