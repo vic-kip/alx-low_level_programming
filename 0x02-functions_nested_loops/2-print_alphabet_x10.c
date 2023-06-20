e068622bc8e4ef728f6626dbaa4e54eabf876ba8
vic-kip <vcherutich19@gmail.com>
2023-06-20 11:08:20 -0700

    printing the alphabet in lowercase and 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  the first while is to create the 10 lines while the nested while is to print a-z lowercase
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+
