985f0b46bf7cc2a01106908226c1653a5fe92800
vic-kip <vcherutich19@gmail.com>
2023-06-20 14:29:59 -0700

    a program that prints the first 50 Fibonacci numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - Lists all the natural numbers below 1024 (excluded)
- *        that are multiples of 3 or 5.
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *        separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2;
-	while (i < 50)
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
 	{
-		if (i == 0)
-		printf("%ld", j);
-	else if (i == 1)
-		printf(", %ld", k);
-	else
-	{
-		k += j;
-		j = k - j;
-		printf(", %ld", k);
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
 	}
-		++i;
-	}
-		printf("\n");
-		return (0);
+
+	return (0);
 }
