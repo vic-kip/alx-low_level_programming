b5083f54da248847c32926e8f362a4bca283bf57
vic-kip <vcherutich19@gmail.com>
2023-06-20 10:50:04 -0700

    prints all the alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+
+	_putchar('\n');
+}
