4606b738f913feafc2d6c04ae03bca3a8cf7a347
vic-kip <vcherutich19@gmail.com>
2023-06-21 11:31:46 -0700

    a program that prints the first 50 Fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the sum of the even-valued terms
- * Return: Always 0 (Success)
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *        separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i;
-	unsigned long int j, k, next, sum;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	j = 1;
-	k = 2;
-	sum = 0;
-
-	for (i = 1; i <= 33; ++i)
+	for (count = 0; count < 50; count++)
 	{
-		if (j < 4000000 && (j % 2) == 0)
-		{
-			sum = sum + j;
-		}
-		next = j + k;
-		j = k;
-		k = next;
-	}
+		sum = fib1 + fib2;
+		printf("%lu", sum);
 
-	printf("%lu\n", sum);
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
 
 	return (0);
 }
