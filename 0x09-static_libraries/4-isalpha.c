4d60d12a6a88598f64294a5fd85d65d9495f8d8d
vic-kip <vcherutich19@gmail.com>
2023-07-07 13:53:02 -0700

    static libraries

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,13 @@
+/**
+ * _isalpha - checks for alphabetic character
+ * @b: the character to be checked
+ * Return: 1 if b is a letter, return 0
+ */
+int _isalpha(int b)
+{
+        if ((b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z'))
+        {
+        return (1);
+        }
+        return (0);
+}
