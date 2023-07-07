d367bdfb0422b2e5cd87e3568b35728bd8aefb6b
vic-kip <vcherutich19@gmail.com>
2023-07-07 11:04:03 -0700

    adds two positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include  <ctype.h>
+/**
+ * main - Print the sum of two numbers
+ * @argc: Count arguments
+ * @argv: Arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int x;
+	int str;
+	int w = 0;
+
+	x = 1;
+	while (x < argc)
+	{
+		if (argv[x])
+
+		{
+			str = atoi(argv[x]);
+			w += str;
+		}
+
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		x++;
+	}
+
+	printf("%d\n", w);
+
+	return (0);
+}
