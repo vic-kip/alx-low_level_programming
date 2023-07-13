4fcac04a77f1688037c3c97211b55d72e57a2a3e
vic-kip <vcherutich19@gmail.com>
2023-07-13 11:26:22 -0700

    allocating memroy using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *x;
+
+	x = malloc(b);
+
+	if (x == NULL)
+		exit(98);
+
+	return (x);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+In this section we will continue to deal with malloc, under 0x0C. C - More malloc, free
