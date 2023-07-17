dc18bf4e80b4e8fee9b974ccba828e65e406b2a6
vic-kip <vcherutich19@gmail.com>
2023-07-17 13:48:35 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * struct dog - contains the dog's basic data
+ * @name: first parameter
+ * @age: Second parameter
+ * @owner: final parameter
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
