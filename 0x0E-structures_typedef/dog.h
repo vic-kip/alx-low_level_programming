fa9a6dc99960a6332f3e3b85b8939b5bc9b4ad8e
vic-kip <vcherutich19@gmail.com>
2023-07-17 14:27:06 -0700

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,5 @@
-#include "main.h"
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - contains the dog's basic data
@@ -12,3 +13,13 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
